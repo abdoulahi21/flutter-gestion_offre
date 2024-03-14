dccc57eecf27858129a1205232c47aa4b9dc3378
Bamba Lo <khadim221lo@gmail.com>
2024-03-14 23:48:30 +0000

    page home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:gestion_offre/pages/HomePage.dart';
+import 'package:gestion_offre/pages/DetailOffre.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title : 'Recherche d\'emploi',
+      home: HomePage(),
       routes: {
 
       },
