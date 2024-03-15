633714f27656c9b417435de1a314926f803a114d
abdoulahi21 <abdoualhi.diallo@unchk.edu.sn>
2024-03-15 11:52:13 +0000

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gestion_offre/pages/OffreDetailsPage.dart';
 import 'package:gestion_offre/pages/welcome.dart';
 import 'package:gestion_offre/pages/CV/MyResumePage.dart';
 
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Welcome(),
+      //home: Welcome(),
+      home: OffreDetailsPage(),
       routes: {
 
       },
